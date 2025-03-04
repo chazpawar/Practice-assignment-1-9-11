7e7ce96a8f4401c13585a1f55df0a5888537433c
Chaitanya Pawar <chiatanya09pawar@gmail.com>
2025-03-04 09:55:00 +0530

    monkeylover boy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react";
+import DestinationContainer from "./components/DestinationContainer";
+
 function App() {
   return (
-    <div>
-      {/* Your code here */}
+    <div className="app-container">
+      <DestinationContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
